2c8d74e590dcdf98c8a15bb339d98d81d5538bd1
Filipe Bacof <filipebacof@gmail.com>
2023-06-03 23:09:04 -0300

    area + espaçamento + moldura

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -2,8 +2,12 @@ type Props = {}
 
 export default function Door({}: Props) {
   return (
-    <div className="flex h-door-hei w-door-wid flex-col items-center bg-orange-700 p-[15px]">
-      <div className="text-5xl">3</div>
+    <div className="flex h-door-hei w-door-wid">
+      <div className="flex grow border-l-[5px] border-r-[5px] border-t-[5px] border-amber-900">
+        <div className="flex grow flex-col items-center bg-orange-600 p-[15px]">
+          <div className="text-5xl">3</div>
+        </div>
+      </div>
     </div>
   )
 }
