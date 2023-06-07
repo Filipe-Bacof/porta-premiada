4765f5746370be5295c8cbbbc16b45f63a9a90b5
Filipe Bacof <filipebacof@gmail.com>
2023-06-07 04:47:21 -0300

    validation on doors form

--- src/app/game/[doors]/[hasgift]/page.tsx
+++ src/app/game/[doors]/[hasgift]/page.tsx
@@ -9,8 +9,19 @@ import DoorModel from '@/model/door'
 import { usePathname } from 'next/navigation'
 
 export default function Game() {
+  const [isValid, setIsValid] = useState(false)
   const [doors, setDoors] = useState<DoorModel[]>([])
   const pathname = usePathname()
+
+  useEffect(() => {
+    const pathPieces = pathname.split('/')
+    const doorsCount = Number(pathPieces[2])
+    const hasGift = Number(pathPieces[3])
+    const validDoors = doorsCount >= 2 && doorsCount <= 30
+    const hasValidGify = hasGift >= 1 && hasGift <= doorsCount
+    setIsValid(validDoors && hasValidGify)
+  }, [doors, pathname])
+
   useEffect(() => {
     const pathPieces = pathname.split('/')
     const doorsCount = Number(pathPieces[2])
@@ -35,7 +46,11 @@ export default function Game() {
   return (
     <div className="flex h-[100vh] flex-col items-center justify-center">
       <div className="flex flex-wrap justify-around self-stretch">
-        {renderDoors()}
+        {isValid ? (
+          renderDoors()
+        ) : (
+          <h2 className="text-[4rem]">Valores Inválidos</h2>
+        )}
       </div>
       <div className="mt-[40px] flex justify-center">
         <Link href="/">
--- src/components/InputNumber.tsx
+++ src/components/InputNumber.tsx
@@ -18,13 +18,13 @@ export default function InputNumber(props: InputNumberPros) {
       <span className="text-[6rem]">{props.value}</span>
       <div className="flex">
         <button
-          className="m-[5px] flex h-[80px] w-[80px] items-center justify-center border-none font-[3rem]"
+          className="m-[5px] flex h-[80px] w-[80px] items-center justify-center border-none bg-slate-200 font-[3rem]"
           onClick={decrement}
         >
           -
         </button>
         <button
-          className="m-[5px] flex h-[80px] w-[80px] items-center justify-center border-none font-[3rem]"
+          className="m-[5px] flex h-[80px] w-[80px] items-center justify-center border-none bg-slate-200 font-[3rem]"
           onClick={increment}
         >
           +
